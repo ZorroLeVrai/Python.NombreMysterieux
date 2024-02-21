ea2c619d69f1056625aa54539d57542dafaca488
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-02-21 16:44:38 +0100

    update

--- user_interface.py
+++ user_interface.py
@@ -4,15 +4,13 @@ from compare_result import CompareResult
 class UserInterface:
     def input_number(self, min_number: int, max_number: int) -> int:
         while (True):
-            input_str = input(f"\nEntrez un nombre entre {
-                              min_number} et {max_number}\n")
+            input_str = input(f"\nEntrez un nombre entre {min_number} et {max_number}\n")
             try:
                 input_number = int(input_str)
                 if (min_number <= input_number <= max_number):
                     return input_number
                 else:
-                    print(f"Le nombre doit être entre {
-                          min_number} et {max_number}\n")
+                    print(f"Le nombre doit être entre {min_number} et {max_number}\n")
             except ValueError:
                 print("Entrez un nombre entier\n")
 
